31c13a8bd8398b044053ce31c76bdd6d05da7157
Steezy <bermudezalejandro45@gmail.com>
2024-01-18 13:42:27 -0500

    Feed layout started

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
+
 // may need to change {} to ()
 const Feed = () => (
   <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
@@ -19,6 +20,15 @@ const Feed = () => (
       >
         Copyright 2024 @bermudez-creates
       </Typography>
+    </Box>
+
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        New
+        <span style={{ color: '#F31503 ' }}> videos</span>
+      </Typography>
+
+      <Videos videos={[]} />
     </Box>
   </Stack>
 );
