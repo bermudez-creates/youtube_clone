9018f3fb9f8871d6d2fb3b06bac58da4bc19ecde
Steezy <bermudezalejandro45@gmail.com>
2024-01-18 16:26:17 -0500

    YouTub API endpoint working

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v311.p.rapidapi.com';
+
+const options = {
+  url: BASE_URL,
+  params: { maxResults: '50' },
+  params: {
+    part: 'snippet',
+    channelId: 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+    maxResults: '5',
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
+
+//'ff5c3a226emsha9d8a344feefa5fp117ea7jsn5b9013c365c6'
