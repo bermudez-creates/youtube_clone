8037614464fb56949e4902ab15d3206e0bf6a1a3
Steezy <bermudezalejandro45@gmail.com>
2024-01-18 13:18:09 -0500

    Sidebar implemented

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 // may need to change {} to ()
-const Feed = () => {
-  return <div>Feed</div>;
-};
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+    <Box
+      sx={{
+        height: { sx: 'auto', md: '94vh' },
+        borderRight: '1px solid #3d3d3d',
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ color: '#fff', mt: 1.5 }}
+      >
+        Copyright 2024 @bermudez-creates
+      </Typography>
+    </Box>
+  </Stack>
+);
 
 export default Feed;
