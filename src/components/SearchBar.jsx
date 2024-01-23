f8c89a192d21b0ed9df2d0930d367a01feb55e08
Steezy <bermudezalejandro45@gmail.com>
2024-01-22 19:13:16 -0500

    Search feed rendered

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,21 @@ import { Paper, IconButton, Icon } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleChange}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -19,8 +30,10 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search clone..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
       />
       <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <Search />
